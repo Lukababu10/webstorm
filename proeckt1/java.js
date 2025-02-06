cbf58396fbf649e4c6bd2b1f4475b41613f01f30
luka <lukababu@gmail.com>
2025-02-06 17:56:18 +0400

    first commit

--- proeckt1/java.js
+++ proeckt1/java.js
@@ -1,21 +1,33 @@
-<script>
-    "use strict";
-    let x = 1;
-    alert( +x ); // 1
-    let y = -2;
-    alert( +y ); // -2
-    // Преобразует не числа в числа
-    alert( +true ); // 1
-    alert( +"" );   // 0
-</script>
+// 1. Switch Statement (with mistakes)
+function getDayName(dayNumber) {
+    switch dayNumber { // Missing parentheses
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        case 7:
+            return "Sunday";
+        default:
+            return "Invalid day number"
+    } // Missing semicolon
+}
+console.log(getDayName(3)) // Missing semicolon
 
-let s = "моя" + "строка";
-alert(s);
+// 2. Function Declaration (with mistakes)
+function add(a, b) {
+    return a + b // Missing semicolon
+    console.log(add(5, 7)); // Function call inside function body
 
-let a = 1;
-let b = 2;
-
-let c = 3 - (a = b + 1);
-
-alert( a ); // 3
-alert( c ); // 0
+// 3. Function Expression (with mistakes)
+    const multiply = function(a, b) {
+        return a * b;
+    };
+    console.log(multiply(4 6)); // Missing comma between arguments
