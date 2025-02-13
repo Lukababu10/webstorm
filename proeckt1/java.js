25ff4d51457f06ba6814b039639023462df12e9a
luka <lukababu@gmail.com>
2025-02-13 17:17:33 +0400

    first commit

--- proeckt1/java.js
+++ proeckt1/java.js
@@ -1,28 +1,19 @@
-function getDayName(dayNumber) {
-    switch dayNumber {
-        case 1:
-            return "Monday";
-        case 2:
-            return "Tuesday";
-        case 3:
-            return "Wednesday";
-        case 4:
-            return "Thursday";
-        case 5:
-            return "Friday";
-        case 6:
-            return "Saturday";
-        case 7:
-            return "Sunday";
-        default:
-            return "Invalid day number"
-    }
+const now = new Date();
+console.log("Current Date and Time:", now.toLocaleString());
+
+const double = num => num * 2;
+console.log("Double of 5:", double(5));
+
+function buggyFunction() {
+    let a = 5;
+    let b = 10;
+    console.log("Before mistake:", a, b);
+
+
+    console.log("Sum:", a + c);
 }
-console.log(getDayName(3))
-function add(a, b) {
-    return a + b
-    console.log(add(5, 7));
-    const multiply = function(a, b) {
-        return a * b;
-    };
-    console.log(multiply(4 6));
+try {
+    buggyFunction();
+} catch (error) {
+    console.error("Error caught:", error);
+}
